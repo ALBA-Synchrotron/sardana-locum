f116fb9226515f5c75255c25bab7cb88ef1d1b6f
AOVya <48925343+AOVya@users.noreply.github.com>
2023-01-19 15:49:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ __version = "1.0.1"
 setup(
     name="sardana_locum",
     version=__version,
+    packages=find_packages(),
     install_requires=["sardana", "setuptools"],
     description="Locum sardaba controller"
 )
